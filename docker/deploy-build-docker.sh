bbc2eb6f70163363182cb78c566615ba342f930a
ferdinands <ferdinand.strobel@infrontfinance.com>
2023-09-05 14:35:13 +0200

    Rework Docker

--- docker/deploy-build-docker.sh
+++ docker/deploy-build-docker.sh
@@ -7,5 +7,10 @@ echo "Build Docker"
 docker build -t plantgrowerangular .
 
 echo "Deploy Docker"
-docker stack deploy -c docker/docker-compose.yml PLANT
+#docker stack deploy -c docker/docker-compose.yml PLANT
+#Stack deploy doesn't work
+
+cd docker
+
+docker-compose up -f
 
